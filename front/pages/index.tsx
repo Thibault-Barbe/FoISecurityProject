74f557cc4684f2a717b892fdeb0715ef16678954
Baptiste Morvan <baptiste.morvan@epitech.eu>
2023-01-15 02:21:53 +0100

    push

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { basename } from 'path';
 import { useState } from 'react';
+import Web3 from 'web3';
+import {abi} from '../ERC20Basic.json';
 
 const Home: NextPage = () => {
   const [accounts, setAccounts] = useState<[string]>([""]);
+  const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:8545/");
 
   const connectMetamask = async () => {
     if (window.ethereum) {
@@ -25,6 +29,18 @@ const Home: NextPage = () => {
     if (address != "" && amount > 0) {
       // TO DO: mettre la fonction web3
     }
+  }
+
+  const getWeb = async() => {
+    var newAbi = JSON.stringify(abi)
+    var contract = new web3.eth.Contract(JSON.parse(newAbi), '0x5fbdb2315678afecb367f032d93f642f64180aa3')
+    console.log(accounts[0])
+    console.log('Balance:')
+    console.log(await contract.methods.balanceOf(accounts[0]).call())
+    console.log(await contract.methods.balanceOf('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266').call())
+    await contract.methods.transferFrom('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266', accounts[0], 10).call()
+    console.log('New Balance:')
+    console.log(await contract.methods.balanceOf(accounts[0]).call())
   }
 
   return (
@@ -52,6 +68,7 @@ const Home: NextPage = () => {
           <button className='py-4 px-10 bg-orange-500 rounded-full w-[40%]' onClick={() => sendToken()}>Send</button>
         </div>
       </div>
+      <button className='py-4 px-10 bg-orange-500 rounded-full w-[40%]' onClick={() => getWeb()}>getWeb</button>
       </>
     ) : (
       <button className='w-[200px] py-7 bg-blue-400 text-white' onClick={() => connectMetamask()}>Connect my wallet</button>
