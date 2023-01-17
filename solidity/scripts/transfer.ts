fc97f4daebdfb3f807c93c58568d347d36f6d56d
Baptiste Morvan <baptiste.morvan@epitech.eu>
2023-01-17 01:04:31 +0100

    updated script

--- solidity/scripts/transfer.ts
+++ solidity/scripts/transfer.ts
@@ -2,12 +2,15 @@ import { ethers } from "hardhat";
 
 const contract = require('../artifacts/contracts/ERC20Basic.sol/ERC20Basic.json')
 const CONTRACT_ADRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
+
+// Transfer values
 const TO_TRANSFER = 10
 const RECEIVER = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199"
+const SENDER_ACCOUNT = 0
 
 async function main() {
     const signers = await ethers.getSigners()
-    const ERC20Contract = new ethers.Contract(CONTRACT_ADRESS, contract.abi, signers[0])
+    const ERC20Contract = new ethers.Contract(CONTRACT_ADRESS, contract.abi, signers[SENDER_ACCOUNT])
     const res = await ERC20Contract.transfer(RECEIVER, TO_TRANSFER)
 
     console.log("transfered: " + TO_TRANSFER + " to " + RECEIVER)
